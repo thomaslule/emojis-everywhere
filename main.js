39e0bdb617d9764bbdbe2a914185129e423a3bc6
unknown <thomas@lule.fr>
2017-06-27 16:31:17 +0200

    refactoring: tray icon to its own class

--- main.js
+++ main.js
@@ -1,63 +1,41 @@
-const { app, Tray, Menu, globalShortcut, dialog } = require( "electron" );
-const path = require( "path" );
+const { app, globalShortcut, dialog } = require( "electron" );
 const AutoLaunch = require( "auto-launch" );
 const EmojisWindow = require( "./emojis-window.js" );
+const TrayIcon = require( "./tray-icon.js" );
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win;
+let win, trayIcon;
 let launcher = new AutoLaunch( { name: "Emojis Everywhere" } );
 
 app.on( "ready", function() {
     win = new EmojisWindow();
-    let trayIcon = createTrayIcon();
-
-    globalShortcut.register( "CommandOrControl+Alt+A", () => win.toggleShow() );
-} );
-
-function createTrayIcon() {
-    trayIcon = new Tray( path.join( __dirname, "build/icon.ico" ) );
-    trayIcon.setToolTip( "Emojis Everywhere" );
-    setContextMenu( trayIcon );
-
-    return trayIcon;
-}
-
-function setContextMenu( trayIcon ) {
     launcher.isEnabled()
         .then(( isEnabled ) => {
-            const contextMenu = Menu.buildFromTemplate( [
+            trayIcon = new TrayIcon( isEnabled,
                 {
-                    label: "Launch at startup",
-                    type: "checkbox",
-                    checked: isEnabled,
-                    click: () => toggleLaunchAtStartup( trayIcon )
-                },
-                {
-                    label: "Show",
-                    click: () => win.show()
-                },
-                {
-                    label: "Quit",
-                    click: app.quit
-                }
-            ] );
-            trayIcon.setContextMenu( contextMenu );
+                    onClickShow: () => win.show(),
+                    onClickQuit: () => app.quit(),
+                    onClickLaunchAtStartup: () => toggleLaunchAtStartup( trayIcon )
+                } );
         } )
         .catch( function( err ) {
             dialog.showErrorBox( "Error", err );
         } );
-}
+
+    globalShortcut.register( "CommandOrControl+Alt+A", () => win.toggleShow() );
+} );
 
 function toggleLaunchAtStartup( trayIcon ) {
     launcher.isEnabled()
         .then(( isEnabled ) => {
             if ( isEnabled ) {
                 launcher.disable()
+                trayIcon.updateContextMenu( false );
             } else {
                 launcher.enable();
+                trayIcon.updateContextMenu( true );
             }
-            setContextMenu( trayIcon );
         } )
         .catch( function( err ) {
             dialog.showErrorBox( "Error", err );
---
+++ tray-icon.js
@@ -0,0 +1,31 @@
+const { Tray, Menu } = require( "electron" );
+const path = require( "path" );
+
+let TrayIcon = function( launchAtStartup, callbacks ) {
+    this.callbacks = callbacks;
+    this.tray = new Tray( path.join( __dirname, "build/icon.ico" ) );
+    this.tray.setToolTip( "Emojis Everywhere" );
+    this.updateContextMenu( launchAtStartup );
+}
+
+TrayIcon.prototype.updateContextMenu = function( launchAtStartup ) {
+    const contextMenu = Menu.buildFromTemplate( [
+        {
+            label: "Launch at startup",
+            type: "checkbox",
+            checked: launchAtStartup,
+            click: this.callbacks.onClickLaunchAtStartup
+        },
+        {
+            label: "Show",
+            click: this.callbacks.onClickShow
+        },
+        {
+            label: "Quit",
+            click: this.callbacks.onClickQuit
+        }
+    ] );
+    this.tray.setContextMenu( contextMenu );
+}
+
+module.exports = TrayIcon;
